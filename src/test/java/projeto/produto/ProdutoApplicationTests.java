2d09f1f954dca6dac029785570a7b1de84874d8c
pehenobra2 <peheguibra@gmail.com>
2024-08-08 11:15:24 -0300

    first commit

---
+++ src/test/java/projeto/produto/ProdutoApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.produto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
